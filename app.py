4d50358fe52fe46c32b6945c3a863c61845dcad1
William <williammaucla@gmail.com>
2020-02-17 14:39:19 -0500

    feat: adding in question answer but deployment too large

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
-
+from question_answer import from_question_return_answer
 
 @app.route('/')
 def base_page():
     return render_template("index.html")
+
+@app.route('/qa')
+def get_question_answer():
+    question, text = "How many moods are there?", "My four moods: I'm too old for this shit! I'm too old for this shit! I'm too sober for this shit! I don't have time for this shit!"
+    return from_question_return_answer(question, text)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- question_answer.py
+++ question_answer.py
@@ -5,7 +5,7 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
 
 
-def question_answer(question, text):
+def from_question_return_answer(question, text):
     input_text = "[CLS]" + question + " [SEP] " + text + " [SEP]"
     input_ids = tokenizer.encode(input_text)
     token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
@@ -13,7 +13,5 @@ def question_answer(question, text):
     all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
     return (' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores) +1]))
 
-
 question, text = "How many moods are there?", "My four moods: I'm too old for this shit! I'm too old for this shit! I'm too sober for this shit! I don't have time for this shit!"
-
-print(question_answer(question, text))
+print(from_question_return_answer(question, text))
