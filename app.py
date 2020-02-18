0aae43323f4579c5828b9349313776cb7bc6bd38
William <williammaucla@gmail.com>
2020-02-17 19:55:57 -0500

    refact: still trying to fix the bug

--- app.py
+++ app.py
@@ -4,16 +4,15 @@ from flask_s3 import FlaskS3
 import flask_s3
 
 app = Flask(__name__)
-"""
-app.config['FLASKS3_BUCKET_NAME'] = 'flaskzappapp'
-s3 = FlaskS3(app)
-flask_s3.create_all(app)
-"""
 
 
 @app.route('/')
 def base_page():
     return render_template("index.html")
+
+@app.route('/myfake')
+def deepFake():
+    return "Hello"
 
 """
 @app.route('/qa')
