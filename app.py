5018330647efa221cb5adfb69994d5fbfc4cb87b
William <williammaucla@gmail.com>
2020-02-29 18:46:54 -0500

    refactor: moving some of the qa code back to qa module

--- app.py
+++ app.py
@@ -1,43 +1,15 @@
 from flask import Flask
-import torch
-import boto3
-from transformers import AlbertTokenizer, AlbertForQuestionAnswering
-import pickle
-from io import BytesIO
+from question_answer import question_answer
+
 
 app = Flask(__name__)
 
-def load_models():
-    s3_bucket = boto3.resource('s3').Bucket('albert-model-files')
-    for object in s3_bucket.objects.all():
-        if object.key in ["config.json", "vocab.txt", "pytorch_model.bin"]:
-            s3_bucket.download_file(object.key, 'model_data/{}'.format(object.key))
-
-    for object in s3_bucket.objects.all():
-        if object.key in ["special_tokens_map.json", "spiece.model", "tokenizer_config.json"]:
-            s3_bucket.download_file(object.key, 'tokenizer_albert/{}'.format(object.key))
-
-    tokenizer = AlbertTokenizer.from_pretrained('./tokenizer_albert')
-    model = AlbertForQuestionAnswering.from_pretrained('./model_data')
-    # model = AlbertForQuestionAnswering.from_pretrained('albert-base-v1')
-    return model, tokenizer
-
-
-def question_answer(question, text):
-    model, tokenizer = load_models()
-    input_dict = tokenizer.encode_plus(question, text, return_tensors="pt")
-    input_ids = input_dict["input_ids"].tolist()
-    start_scores, end_scores = model(**input_dict)
-
-    all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
-    answer = ''.join(all_tokens[torch.argmax(start_scores): torch.argmax(end_scores) + 1]).replace('▁', ' ').strip()
-    return answer
-
 
 @app.route('/')
-def test_qa():
+def qa():
     question, text = "How many moods are there?", "My four moods: I'm too old for this shit! I'm too old for this shit! I'm too sober for this shit! I don't have time for this shit!"
     return(question_answer(question, text))
 
-if __name__ == '__main__':
-    print(test_qa())
+
+if __name__ == "__main__":
+    print(qa())
--- question_answer.py
+++ question_answer.py
@@ -1,55 +1,35 @@
-from transformers import *
 import torch
 import boto3
-import pickle
-
-def question_answer(question, text, model, tokenizer):
-    input_text = "[CLS]" + question + " [SEP] " + text + " [SEP]"
-    input_ids = tokenizer.encode(input_text)
-    token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
-    start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
-    all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
-    return (' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores) +1]))
+from transformers import AlbertTokenizer, AlbertForQuestionAnswering
+import os
 
 
+def load_models():
+    # Download files locally if not exist from S3 bucket
+    s3_bucket = boto3.resource('s3').Bucket('albert-model-files')
+    for object in s3_bucket.objects.all():
+        if object.key in ["config.json", "vocab.txt", "pytorch_model.bin"]:
+            if not os.path.exists('model_data/{}'.format(object.key)):
+                s3_bucket.download_file(object.key, 'model_data/{}'.format(object.key))
 
-if __name__ == "__main__":
-    """
-        tokenizer = BertTokenizer.from_pretrained('./tokenizer_albert')
-    model = BertForQuestionAnswering.from_pretrained('./model_data')
-    print(question_answer("Who is John?", "I eat pizza. John is a criminal", model, tokenizer))
-    bucketname = 'qaexample'  # replace with your bucket name
-    s3 = boto3.resource('s3').Bucket(bucketname)
-    # s3.Bucket(bucketname).download_file("special_tokens_map.json", 'tokenizer_albert/coasdfaig1.json')
+    for object in s3_bucket.objects.all():
+        if object.key in ["special_tokens_map.json", "spiece.model", "tokenizer_config.json"]:
+            if not os.path.exists('tokenizer_albert/{}'.format(object.key)):
+                s3_bucket.download_file(object.key, 'tokenizer_albert/{}'.format(object.key))
 
-        for object in s3.objects.all():
-        if object.key in ["special_tokens_map.json", "tokenizer_config.json", "vocab.txt"]:
-            s3.download_file(object.key, 'tokenizer_albert/{}'.format(object.key))
-        elif object.key in ["config.json", "pytorch_model.bin"]:
-            s3.download_file(object.key, 'model_data/{}'.format(object.key))
-            """
-            
-            
-    # model = AutoModel.from_pretrained("distilbert-base-multilingual-cased")
-
-    # model = BertForQuestionAnswering.from_pretrained('distilbert-base-uncased-distilled-squad')
-    # model.save_pretrained("/home/william/Documents/repos/turbo-invention/model_data/")
-
-    from transformers import AlbertTokenizer, AlbertForQuestionAnswering
-    import torch
-
+    # Load pretrained models
     tokenizer = AlbertTokenizer.from_pretrained('./tokenizer_albert')
-    # tokenizer.save_pretrained("./tokenizer_albert")
     model = AlbertForQuestionAnswering.from_pretrained('./model_data')
-    # model = AlbertForQuestionAnswering.from_pretrained('albert-base-v2')
+    return model, tokenizer
 
-    question, text = "How many moods are there?", "My four moods: I'm too old for this shit! I'm too cold for this shit! I'm too sober for this shit! I don't have time for this shit!"
 
+def question_answer(question, text):
+    # Torch code to return output from pretrained models
+    model, tokenizer = load_models()
     input_dict = tokenizer.encode_plus(question, text, return_tensors="pt")
     input_ids = input_dict["input_ids"].tolist()
     start_scores, end_scores = model(**input_dict)
 
     all_tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
     answer = ''.join(all_tokens[torch.argmax(start_scores): torch.argmax(end_scores) + 1]).replace('▁', ' ').strip()
-    print(answer)
-
+    return answer
